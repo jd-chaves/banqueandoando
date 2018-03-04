cefc8a92ecef22430d11f5bb131ac82f189444d8
Andrés <af.olivares10@uniandes.edu.co>
2018-03-04 01:56:03 -0500

    Insertar nuevos comentarios

--- frontend/src/ModalReferencias.js
+++ frontend/src/ModalReferencias.js
@@ -8,25 +8,32 @@ class ModalReferencias extends Component {
       referencias: []
     };
   }
-componentDidMount ()
-{
-  fetch('http://localhost:8080/api/referencias/'+ this.props.banco)
-  .then((response) => response.json())
-  .then((responseJson) => {
-    if(responseJson.success){
-      this.setState({referencias:responseJson.referencias});
-    }
-    else{
-      console.log(responseJson);
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-}
-mostrarReferencia = (ref, i) =>{
-return (<Comentario nombre = {ref.nombre} texto = {ref.texto} puntos = {ref.puntos} key = {i}/>)
-}
+  componentDidMount ()
+  {
+    fetch('http://localhost:8080/api/referencias/'+ this.props.banco)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if(responseJson.success){
+        this.setState({referencias:responseJson.referencias});
+      }
+      else{
+        console.log(responseJson);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+  mostrarReferencia = (ref, i) =>{
+    return (
+    <div>
+      <Comentario nombre = {ref.nombre} texto = {ref.texto} puntos = {ref.puntos} key = {i}/>
+      <hr/>
+
+    </div>
+
+    )
+  }
   render() {
     return (
       <div className="container">
@@ -38,11 +45,14 @@ return (<Comentario nombre = {ref.nombre} texto = {ref.texto} puntos = {ref.punt
                 <h4 className="modal-title">{ this.props.banco}</h4>
               </div>
               <div className="modal-body">
+
                 {this.state.referencias.map(this.mostrarReferencia)}
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
               </div>
+
+
             </div>
           </div>
         </div>
--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Icon, Card } from 'antd';
 import ModalReferencias from './ModalReferencias';
+import ModalNuevaReferencia from './ModalNuevaReferencia';
+
 import { Rate } from 'antd';
 const { Meta } = Card;
 
 class Results extends React.Component {
   idSin = this.props.bankName.replace(/\s+/g, '').split('.').join("");
   id = "#"+this.idSin;
-
+  idSinr = this.props.bankName.replace(/\s+/g, '').split('.').join("")+"r";
+  idr = "#"+this.idSinr;
   render(){
     return(
       <div>
         <Card
           style={{ width: "20vw" }}
           cover={<img alt="logo banco" src={this.props.imgUrl} style={{ height: "22vh", padding:"5px"}} />}
-          actions={[<button type="button" className="btn btn-info " data-toggle="modal"  data-target={this.id}><span className="glyphicon glyphicon-plus"></span></button>, <button type="button" className="btn btn-info " data-toggle="modal" data-target={this.id}><span className="glyphicon glyphicon-pencil"></span></button>]}
+          actions={[<button type="button" className="btn btn-info " data-toggle="modal"  data-target={this.id}><span className="glyphicon glyphicon-plus"></span></button>, <button type="button" className="btn btn-info " data-toggle="modal" data-target={this.idr}><span className="glyphicon glyphicon-pencil"></span></button>]}
           >
 
             <Meta
@@ -22,6 +25,8 @@ class Results extends React.Component {
               description={[<Icon type="calculator" style={{ marginRight: "4px"}}/>, this.props.interes, <Rate disabled defaultValue={this.props.rate} style={{ float: "right", marginTop:"-5px", paddingRight:"-40px" }}/>]}            />
             </Card>
             <ModalReferencias banco ={this.props.bankName} id ={this.idSin} />
+            <ModalNuevaReferencia banco ={this.props.bankName} id ={this.idSinr} />
+
           </div>
         );
       }
