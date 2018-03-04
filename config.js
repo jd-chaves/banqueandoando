ecb82a069d9396ff95a9601c418e8ad7ac51b1aa
ramonvega96 <re.vega11@uniandes.edu.co>
2018-03-04 11:02:38 -0500

    app.json

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
  module.exports = {
  	'port': process.env.PORT || 8080,
- 	'database': 'mongodb://moncho:moncho@ds155268.mlab.com:55268/proyecto2web',
-  //'database': 'mongodb://localhost:27017/pmc',
+ 	'database': process.env.MONGOLAB_URI,
   'secret': 'ilovescotchscotchyscotchscotch'
  };
